e7a965a4ebab59829351622d6af5e4402b7506a4
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-29 16:37:40 -0700

    add total of 5 successful tests, the 4 new ones are for getCost method and verifyCoupon methods

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,32 @@
 public class Event {
   private int mGuestCount;
   private String mFoodServed;
+  private String mBevServed;
   private String mEntertainment;
+  private int mCost;
 
-  public Event(int guestCount, String foodServed, String entertainment){
+  public Event(int guestCount, String foodServed, String bevServed, String entertainment){
     mGuestCount = guestCount;
     mFoodServed = foodServed;
+    mBevServed = bevServed;
     mEntertainment = entertainment;
+    mCost = 0;
   }
+
+  public int getCost() {
+    return mCost;
+  }
+
+  public int verifyCoupons(String coupon1, String coupon2) {
+    int couponCount = 0;
+    if(coupon1.equals("216F&B") || coupon1.equals("780K#L")) {
+      couponCount++;
+    }
+    if(coupon2.equals("216F&B") || coupon2.equals("780K#L")) {
+      couponCount++;
+    }
+    return couponCount;
+  }
+
+
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,8 +4,34 @@ import static org.junit.Assert.*;
 public class EventTest {
 
   @Test
-  public void newEvent_instantiatesCorrectly() {
-    Event testEvent = new Event(15, "Steak", "DJ");
+  public void newEvent_instantiatesCorrectly_true() {
+    Event testEvent = new Event(15, "Steak", "Wine & Beer", "DJ");
     assertEquals(true, testEvent instanceof Event);
   }
+
+  @Test
+  public void getCost_returnCost_0() {
+    Event testEvent = new Event(15, "Steak", "Wine & Beer", "DJ");
+    assertEquals(0, testEvent.getCost());
+  }
+
+  @Test
+  public void verifyCoupons_noValidCouponCodes_0() {
+    Event testEvent = new Event(15, "Steak", "Wine & Beer", "DJ");
+    assertEquals(0, testEvent.verifyCoupons("27GH$X" , "45NH$V"));
+  }
+
+  @Test
+  public void verifyCoupons_oneValidCouponCode_1() {
+    Event testEvent = new Event(15, "Steak", "Wine & Beer", "DJ");
+    assertEquals(1, testEvent.verifyCoupons("27GH$X" , "780K#L"));
+  }
+
+  @Test
+  public void verifyCoupons_twoValidCouponCode_2() {
+    Event testEvent = new Event(15, "Steak", "Wine & Beer", "DJ");
+    assertEquals(2, testEvent.verifyCoupons("216F&B" , "780K#L"));
+  }
+
+
 }
