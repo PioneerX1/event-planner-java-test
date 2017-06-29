680a9c8a5014226393b28ed908ea47861a721a4f
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-29 16:02:53 -0700

    add first successful test for instantiation of Event object

---
+++ src/main/java/Event.java
@@ -0,0 +1,11 @@
+public class Event {
+  private int mGuestCount;
+  private String mFoodServed;
+  private String mEntertainment;
+
+  public Event(int guestCount, String foodServed, String entertainment){
+    mGuestCount = guestCount;
+    mFoodServed = foodServed;
+    mEntertainment = entertainment;
+  }
+}
