70e0e30827bcad73207f426763ed6b7342b28299
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-29 18:15:02 -0700

    add more tests for different subcosts like food, entertainment, and bevvies, also calculateTotalCost method and tests to start checking for coupons

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,13 +4,62 @@ public class Event {
   private String mBevServed;
   private String mEntertainment;
   private int mCost;
+  private boolean mValidCoupon1;
+  private boolean mValidCoupon2;
 
-  public Event(int guestCount, String foodServed, String bevServed, String entertainment){
+  public Event(int guestCount, String foodServed, String bevServed, String entertainment) {
     mGuestCount = guestCount;
     mFoodServed = foodServed;
     mBevServed = bevServed;
     mEntertainment = entertainment;
     mCost = 0;
+    mValidCoupon1 = false;
+    mValidCoupon2 = false;
+  }
+
+  public int calculateTotalCost() {
+    mCost = this.calculateFoodCost() + this.calculateBevCost() + this.calculateEntertainmentCost();
+    //return mCost;
+    if(mValidCoupon1 == true) {
+      mCost = mCost - 50;
+    }
+    if(mValidCoupon2 == true && mEntertainment.equals("DJ") && mGuestCount >= 150) {
+      mCost = mCost - this.calculateEntertainmentCost();
+    }
+
+    return mCost;
+  }
+
+  public int calculateFoodCost() {
+    if (mFoodServed.equals("Steak")) {
+      return mGuestCount * 30;
+    } else if (mFoodServed.equals("Fish")) {
+      return mGuestCount * 20;
+    } else {
+      return mGuestCount * 10;
+    }
+  }
+
+  public int calculateBevCost() {
+    if (mBevServed.equals("Full Bar")) {
+      return mGuestCount * 30;
+    } else if (mBevServed.equals("Wine & Beer")) {
+      return mGuestCount * 15;
+    } else {
+      return mGuestCount * 5;
+    }
+  }
+
+  public int calculateEntertainmentCost() {
+    if (mEntertainment.equals("DJ")) {
+      return 300;
+    } else if (mEntertainment.equals("Fire Breathers")) {
+      return 800;
+    } else if (mEntertainment.equals("Mime Flash Mob")) {
+      return 1500;
+    } else {
+      return 0;
+    }
   }
 
   public int getCost() {
@@ -19,26 +68,16 @@ public class Event {
 
   public boolean verifyCoupon1(String coupon) {
     if (coupon.equals("216F&B")) {
+      mValidCoupon1 = true;
       return true;
     } else { return false; }
   }
 
   public boolean verifyCoupon2(String coupon) {
     if (coupon.equals("780K#L")) {
+      mValidCoupon2 = true;
       return true;
     } else { return false; }
   }
-
-  // public int verifyCoupons(String coupon1, String coupon2) {
-  //   int couponCount = 0;
-  //   if(coupon1.equals("216F&B") || coupon1.equals("780K#L")) {
-  //     couponCount++;
-  //   }
-  //   if(coupon2.equals("216F&B") || coupon2.equals("780K#L")) {
-  //     couponCount++;
-  //   }
-  //   return couponCount;
-  // }
-
 
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -39,6 +39,79 @@ public class EventTest {
     assertEquals(true, testEvent.verifyCoupon2("780K#L"));
   }
 
+  @Test
+  public void calculateFoodCost_forSteakOption_450() {
+    Event testEvent = new Event(15, "Steak", "Wine & Beer", "DJ");
+    assertEquals(450, testEvent.calculateFoodCost());
+  }
+
+  @Test
+  public void calculateFoodCost_forFishOption_300() {
+    Event testEvent = new Event(15, "Fish", "Wine & Beer", "DJ");
+    assertEquals(300, testEvent.calculateFoodCost());
+  }
+
+  @Test
+  public void calculateFoodCost_forVeggieOption_150() {
+    Event testEvent = new Event(15, "Veggies", "Wine & Beer", "DJ");
+    assertEquals(150, testEvent.calculateFoodCost());
+  }
+
+  @Test
+  public void calculateBevCost_forFullBar_450() {
+    Event testEvent = new Event(15, "Veggies", "Full Bar", "DJ");
+    assertEquals(450, testEvent.calculateBevCost());
+  }
+
+  @Test
+  public void calculateBevCost_forWineAndBeer_225() {
+    Event testEvent = new Event(15, "Veggies", "Wine & Beer", "DJ");
+    assertEquals(225, testEvent.calculateBevCost());
+  }
+
+  @Test
+  public void calculateBevCost_forSoftDrinksAndJuice_75() {
+    Event testEvent = new Event(15, "Veggies", "Soft Drinks & Juice", "DJ");
+    assertEquals(75, testEvent.calculateBevCost());
+  }
+
+  @Test
+  public void calculateEntertainmentCost_forDJ_300() {
+    Event testEvent = new Event(15, "Veggies", "Full Bar", "DJ");
+    assertEquals(300, testEvent.calculateEntertainmentCost());
+  }
+
+  @Test
+  public void calculateEntertainmentCost_forFireBreathers_300() {
+    Event testEvent = new Event(15, "Veggies", "Full Bar", "Fire Breathers");
+    assertEquals(800, testEvent.calculateEntertainmentCost());
+  }
+
+  @Test
+  public void calculateEntertainmentCost_forMimeFlashMob_1500() {
+    Event testEvent = new Event(15, "Veggies", "Full Bar", "Mime Flash Mob");
+    assertEquals(1500, testEvent.calculateEntertainmentCost());
+  }
+
+  @Test
+  public void calculateTotalCost_for15VeggiesFullBarMimeFlashMob_2100() {
+    Event testEvent = new Event(15, "Veggies", "Full Bar", "Mime Flash Mob");
+    assertEquals(2100, testEvent.calculateTotalCost());
+  }
+
+  @Test
+  public void calculateTotalCost_for100FishWineAndBeerMimeFlashMob_5000() {
+    Event testEvent = new Event(100, "Fish", "Wine & Beer", "Mime Flash Mob");
+    assertEquals(5000, testEvent.calculateTotalCost());
+  }
+
+  @Test
+  public void calculateTotalCost_for150and2ndCouponFreeDJ_9000() {
+    Event testEvent = new Event(150, "Steak", "Full Bar", "DJ");
+    boolean coupon2Legit = testEvent.verifyCoupon2("780K#L");
+    assertEquals(9000, testEvent.calculateTotalCost());
+  }
+
 
 
 }
